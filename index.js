5f50ba8209bc9e4ed92e3e40d5e980065cef67cc
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-12 21:33:22 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (superbowls) => {
+  let superbowl = superbowls.find( record => record.result === "W" );
+  return !!superbowl ? superbowl.year : undefined
+}
+
+
